69836d93cb03f36cc9eb57daa8fa06ece7715e72
grbalmeida <g.r.almeida@live.com>
2023-10-16 10:31:13 -0300

    Organizando o código

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -6,7 +6,9 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
+import br.com.alura.loja.dao.ProdutoDao;
 import br.com.alura.loja.modelo.Produto;
+import br.com.alura.loja.util.JPAUtil;
 
 public class CadastroDeProduto {
 
@@ -17,15 +19,13 @@ public class CadastroDeProduto {
 		celular.setDescricao("Muito legal");
 		celular.setPreco(new BigDecimal("800"));
 		
-		EntityManagerFactory factory = Persistence
-				.createEntityManagerFactory("loja");
-		
-		EntityManager em = factory.createEntityManager();
+		EntityManager em = JPAUtil.getEntityManager();
+		ProdutoDao dao = new ProdutoDao(em);
 		
 		// Como não está usando JTA, e o servidor não gerencia automaticamente a transação
 		// tem que iniciar e commitar a transação
 		em.getTransaction().begin();
-		em.persist(celular);
+		dao.cadastrar(celular);
 		em.getTransaction().commit();
 		em.close();
 		
