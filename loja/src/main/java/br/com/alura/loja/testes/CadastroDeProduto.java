37c9897ae3576a624084f0fbce5bfc274bc88d57
grbalmeida <g.r.almeida@live.com>
2023-10-16 10:45:25 -0300

    Mais mapeamentos

--- loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
+++ loja/src/main/java/br/com/alura/loja/testes/CadastroDeProduto.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 import br.com.alura.loja.dao.ProdutoDao;
+import br.com.alura.loja.modelo.Categoria;
 import br.com.alura.loja.modelo.Produto;
 import br.com.alura.loja.util.JPAUtil;
 
@@ -14,10 +15,7 @@ public class CadastroDeProduto {
 
 	public static void main(String[] args) {
 		
-		Produto celular = new Produto();
-		celular.setNome("Xiaomi Redmi");
-		celular.setDescricao("Muito legal");
-		celular.setPreco(new BigDecimal("800"));
+		Produto celular = new Produto("Xiaomi Redmi", "Muito legal", new BigDecimal("800"), Categoria.CELULARES);
 		
 		EntityManager em = JPAUtil.getEntityManager();
 		ProdutoDao dao = new ProdutoDao(em);
